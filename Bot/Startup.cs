8093a2534dd25098668fa8baeb14f0ae6a3d9dd2
Oleg Tsomkalov <infinitu1327@gmail.com>
2021-09-17 13:19:13 +0300

    Add AppInsights logging (#5)

--- Bot/Startup.cs
+++ Bot/Startup.cs
@@ -23,6 +23,8 @@ namespace RatingsBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
+
             services.AddDbContext<AppDbContext>(builder =>
                 builder
                     .UseLazyLoadingProxies()
